5e998844bc27f7e2e68d32886a008f874f310363
snwc0de <154846385+snwc0de@users.noreply.github.com>
2025-01-01 23:39:04 +0100

    More array methods

--- js-objects/array-in-object/script.js
+++ js-objects/array-in-object/script.js
@@ -1,14 +1,5 @@
-function sumOfTripleEven (array) {
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] % 2 === 0) {
-            const tripleEven = array[i] * 3;
-            
-            sum += tripleEven;
-        }
-    }
-    return sum;
-}
+
+const arr = [1, 2, 3, 4, 5];
 
 
 //MAP METHOD
@@ -16,53 +7,80 @@ function addOne (num) {
     return num + 1;
 };
 
-const arr = [1, 2, 3, 4, 5];
-const mapArr = arr.map(addOne);  //map iterates through the array and 
-                                 //applies the callback function to each array element 
+const mapArr = arr.map(addOne);  //map iterates through the array and applies the callback fn to each array element 
                                  //produces a new array without altering the original array.
 
 console.log(arr);       //[1, 2, 3, 4, 5] (unchanged)
-console.log(mapArr);    //[2, 3, 4, 5, 6] (new array)
+console.log(mapArr);    //[2, 3, 4, 5, 6] (new array, increments each arr element by 1)
 //OR
-const arr1 = [1, 2, 3, 4, 5];
 const mapArr1 = arr.map((element) => element + 1);
 
-console.log(arr1);      //[1, 2, 3, 4, 5] (unchanged)
 console.log(mapArr1);   //[2, 3, 4, 5, 6] (same as first method)
 
 
 //FILTER METHOD
-//also iterates through the loop and applies the callback function to each array element
-//but instead it returns the original value, only if the callback function returns true
 function isOddNo (num) {
     return num % 2 !== 0;
 }
+ 
+const filt = arr.filter(isOddNo);  //filter iterates through the array and applies the callback fn to each array element
+                                   //returns the original value, only if the callback function returns true
 
-const arrD = [1, 2, 3, 4, 5];   
-const filt = arrD.filter(isOddNo);  //filter iterates through the array and
-                                    //applies the callback function to each array element
-                                    //returns the original value, only if the callback function returns true
-
-console.log(arrD);  //[1, 2, 3, 4, 5] (unchanged)
-console.log(filt);  //[1, 3, 5] (filters odd)
+console.log(arr);   //[1, 2, 3, 4, 5] (unchanged)
+console.log(filt);  //[1, 3, 5] (filters odd numbers)
 //OR
-const arrD1 = [1, 2, 3, 4, 5];
-const filt1 = arrD1.filter((element) => element % 2 !== 0);
+const filt0 = arr.filter((element) => element % 2 !== 0);
 
-console.log(arrD1);  //[1, 2, 3, 4, 5] (unchanged)
-console.log(filt1);  //[1, 3, 5] (same as first method)
+console.log(filt0);  //[1, 3, 5] (same as first method)
 
-//REDUCE METHOD
-//It iterates through the array and it has two arguments and is used with an operator in the callback function
-//the first argument is the accumulator (total) and is the intialValue or the first element of array if no initialValue is provided
-//second argument is the second operand, it is the current value which is being iterated on
-//an initialValue is set as the second argument (after the callback fn), when we don't want the initial value set to the first element of the array
 
-const set = [4, 5, 6, 7, 8, 9];
-const multiple = set.reduce((one, two) => one * two, 5);    //without initialValue
-const multiples = set.reduce((one, two) => one * two, 5)    //with initialValue, the total after callback fn is multiplied by 5
-const summation = set.reduce((first, second) => first + second);    
+//REDUCE and REDUCE-RIGHT METHOD
+                                                        //iterates through the array, has two arguments and is used with an operator in the callback fn
+const time = arr.reduce((one, two) => one * two);       //1st-arg (total accumulator) = intialValue or the 1st-array-element if no initialValue is set
+const times = arr.reduce((one, two) => one * two, 5);   //2nd-arg (second operand) = current value which is being iterated on
+const sum = arr.reduce((one, two) => one + two);        //initialValue = 2nd arg(after callback fn), if we don't want the initial value set to the 1st-array-element
+const rg = arr.reduceRight((one, two) => one - two);    //same as reduce but starts from the right
 
-console.log(set);
-console.log(multiple);
-console.log(summation);
+console.log(time);    //120, no initialValue
+console.log(times);   //600, multiplies the initialValue by 5
+console.log(sum);     //15, adds all arr elements together
+console.log(rg);      //-5, subtracts all elements from the right
+
+
+function sumOfTripleEven (array) {
+    return array
+        .filter((num) => num % 2 === 0)                 //selects each even number array item
+        .map((num) => num * 3)                          //multiplies each even number by 3
+        .reduce((total, current) => total + current);   //add all the array items together
+};
+
+
+//FOR EACH METHOD
+const eachItem = arr.forEach((element, index, array) => {           //iterates over the array and applies the callback fn to each element
+    console.log(`${element} is at index ${index} of ${array}`);     //does not create a new array and does not return a value
+});                                                                 //It mutates the original array
+
+const each = arr.forEach((element) => {
+    console.log(element * 2);
+});
+
+
+//INDEX-OF, LAST-INDEX-OF AND INCLUDES METHOD
+let arrr = [1, 0, false, 1];
+
+console.log( arrr.indexOf(false) ); // 2, indexOf returns the index position of the specified array element.
+
+console.log( arrr.lastIndexOf(1)); // 3, lastIndexOf returns the index position of the specified array element starting from behind.
+
+console.log( arrr.includes(0) ); // true, includes looks for the specified array element, and returns true if found, it uses (===).
+
+
+//SORT
+let mix = [1, 39, 15, 2];              //sort arranges the element in order, they are sorted by default as string
+
+let fix = mix.sort((a, b) => a - b);   //callback fn fixes that
+
+console.log(fix);
+
+
+
